8292fbaf0abaadbe4337eaac0dd3e32a55097d60
Leo <Leo@Users-MacBook-Pro-4.local>
2020-04-13 12:20:49 +0100

    Float formatting

---
+++ formatting.py
@@ -0,0 +1,9 @@
+# This program demostrates how a floating-point numnber can be formatted.
+
+amount_due = 5000.0
+
+monthly_payment = amount_due /12
+
+print('The monthly payment is', format(monthly_payment, '.2f'))
+
+
